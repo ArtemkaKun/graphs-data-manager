0777c0bb8e35e9843a66d2c6a6d8696cfdbef60d
Artem Yurchenko <zumikq78@gmail.com>
2021-07-15 21:57:12 +0200

    TODOs were added to ConsoleUI class

--- GraphsDataManager/ConsoleUI.cs
+++ GraphsDataManager/ConsoleUI.cs
@@ -5,6 +5,10 @@ namespace GraphsDataManager
 	public class ConsoleUI
 	{
 		private const string WELCOME_MESSAGE = "Programm was started successfully. Waiting for user commands.";
+		//TODO DICTIONARY COMMAND-Action
+		//TODO Create Command attribute to mark command methods
+		//TODO create description attribute for commands
+		//TODO create HelpClass object
 		
 		public void Start ()
 		{
@@ -12,7 +16,7 @@ namespace GraphsDataManager
 			
 			while (true)
 			{
-				
+				//TODO implement commands handling
 			}
 		}
 	}
