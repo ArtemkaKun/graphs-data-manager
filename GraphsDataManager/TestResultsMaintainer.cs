b0fede79c61305105ae60f3ef74385e90188d042
Artem Yurchenko <zumikq78@gmail.com>
2021-07-16 21:03:33 +0200

    ReadLogData function was implemented

--- GraphsDataManager/TestResultsMaintainer.cs
+++ GraphsDataManager/TestResultsMaintainer.cs
@@ -1,10 +1,23 @@
-﻿namespace GraphsDataManager
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+
+namespace GraphsDataManager
 {
 	public class TestResultsMaintainer
 	{
 		public void StartConversion (string pathToLog, string pathToStoreResult)
 		{
-			
+			List<LogData> logRecords = ReadLogData(pathToLog);
+		}
+
+		private List<LogData> ReadLogData (string pathToLog)
+		{
+			using StreamReader reader = new(pathToLog);
+			using CsvReader csv = new(reader, CultureInfo.InvariantCulture);
+			return csv.GetRecords<LogData>().ToList();
 		}
 	}
 }
