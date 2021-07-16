d22d447ad43bab3d00e4eb191e63417faf7f6196
Artem Yurchenko <zumikq78@gmail.com>
2021-07-16 20:59:19 +0200

    LogData class was implemented

---
+++ GraphsDataManager/LogData.cs
@@ -0,0 +1,12 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace GraphsDataManager
+{
+	public class LogData
+	{
+		[Name("TimeInSeconds")]
+		public double TimeInSeconds { get; set; }
+		[Name("MsBetweenDisplayChange")]
+		public double MsBetweenDisplayChange { get; set; }
+	}
+}
