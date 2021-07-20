6a37bdf02bb92c542df7908694031b563162f0df
Artem Yurchenko <zumikq78@gmail.com>
2021-07-20 20:47:10 +0200

    LogData class was created

---
+++ GraphsDataManager/LogConversionSystems/ResultsDataCalculationSystems/LogData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace GraphsDataManager.LogConversionSystems.ResultsDataCalculationSystems
+{
+	public class LogData
+	{
+		public string LogFileName { get; }
+		public List<LogDataRecord> LogRecords { get; }
+
+		public LogData (string logFileName, List<LogDataRecord> logRecords)
+		{
+			LogFileName = logFileName;
+			LogRecords = logRecords;
+		}
+	}
+}
